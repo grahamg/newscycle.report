daaac15cf55e6a354e42492069f1117cc0b937d0
Graham Greenfield <grahamg@gmail.com>
2024-07-09 04:59:51 -0500

    In bookmarks template add links for exporting contents to raw text, json or xml.

--- bulletlist_report/feed/urls.py
+++ bulletlist_report/feed/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.contrib.auth import views as auth_views
 from . import views as feed_views
 
 urlpatterns = [
     path('', feed_views.index, name='index'),
     path('~<str:username>/', feed_views.bookmarks, name='bookmarks'),
+    re_path(r'^~(?P<username>[\w.@+-]+)/(?P<format>(raw|json|xml))/$', feed_views.bookmarks_format, name='bookmarks_format'),
     path('config/', feed_views.config, name='config'),
     path('api/v1/bookmark/', feed_views.APIBookmarkActionView.as_view(), name='api_bookmark'),
 ]
--- bulletlist_report/feed/views.py
+++ bulletlist_report/feed/views.py
@@ -1,10 +1,12 @@
 import feedparser
+import xml.etree.ElementTree as ET
 
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 from django.db.models import ObjectDoesNotExist
+from django.http import HttpResponseNotFound, HttpResponse, JsonResponse
 from django.db import IntegrityError
 
 from rest_framework.views import APIView
@@ -80,6 +82,51 @@ def bookmarks(request, username):
     bookmarks = UserBookmark.objects.filter(user=url_user, visible=True).order_by('-added_on')
     context = {'url_username': username, 'bookmarks': bookmarks}
     return render(request, 'bookmarks.html', context)
+
+def bookmarks_format(request, username, format):
+    try:
+        url_user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        return HttpResponseNotFound("<h1>User not found.</h1>")
+
+    bookmarks = UserBookmark.objects.filter(user=url_user, visible=True).order_by('-added_on')
+
+    if format == 'json':
+        bookmarks_data = []
+        for bookmark in bookmarks:
+            bookmarks_data.append({
+                "title": bookmark.rss_feed_item.title,
+                "link": bookmark.rss_feed_item.link,
+                "added_on": bookmark.added_on.strftime("%Y-%m-%d %H:%M:%S"),
+            })
+        return JsonResponse({"bookmarks": bookmarks_data})
+
+    elif format == 'xml':
+        root = ET.Element("bookmarks")
+        for bookmark in bookmarks:
+            bookmark_element = ET.SubElement(root, "bookmark")
+            title_element = ET.SubElement(bookmark_element, "title")
+            title_element.text = bookmark.rss_feed_item.title
+            link_element = ET.SubElement(bookmark_element, "link")
+            link_element.text = bookmark.rss_feed_item.link
+            added_on_element = ET.SubElement(bookmark_element, "added_on")
+            added_on_element.text = bookmark.added_on.strftime("%Y-%m-%d %H:%M:%S")
+        xml_string = ET.tostring(root, encoding="utf-8", method="xml")
+        return HttpResponse(xml_string, content_type="application/xml")
+
+    elif format == 'raw':
+        text_buffer = ""
+        for bookmark in bookmarks:
+            added_on = bookmark.added_on.strftime("%Y-%m-%d %H:%M:%S")
+            text_buffer += f"Title: {bookmark.rss_feed_item.title}\n"
+            text_buffer += f"Link: {bookmark.rss_feed_item.link}\n"
+            text_buffer += f"Added on: {added_on}\n"
+            text_buffer += "\n"
+        return HttpResponse(text_buffer, content_type="text/plain")
+
+    else:
+        return HttpResponseNotFound("<h1>Invalid response format requested.</h1>")
+
 
 class APIBookmarkActionView(APIView):
     permission_classes = [IsAuthenticated]
