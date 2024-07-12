7caf0b00b4bba0bb521ffd0ab26ee52c6c646f5b
Graham Greenfield <grahamg@gmail.com>
2024-07-12 05:29:46 -0500

    Improve styling and feedback on user registration form.

--- bulletlist_report/user/forms.py
+++ bulletlist_report/user/forms.py
@@ -15,15 +15,21 @@ class CustomAuthenticationForm(AuthenticationForm):
     )
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={
-        'class': 'w-full px-4 py-2 border border-black rounded-md'
-    }))
-    
+    password1 = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(attrs={'class': 'w-full px-4 py-2 border border-black rounded-md', 'autocomplete': 'new-password'}),
+        help_text='Your password must be at least 8 characters long and should not be entirely numeric.',
+    )
+    password2 = forms.CharField(
+        label='Password confirmation',
+        widget=forms.PasswordInput(attrs={'class': 'w-full px-4 py-2 border border-black rounded-md', 'autocomplete': 'new-password'}),
+        help_text='Enter the same password as above, for verification.'
+    )
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
         widgets = {
             'username': forms.TextInput(attrs={'class': 'w-full px-4 py-2 border border-black rounded-md'}),
-            'password1': forms.PasswordInput(attrs={'class': 'w-full px-4 py-2 border border-black rounded-md'}),
-            'password2': forms.PasswordInput(attrs={'class': 'w-full px-4 py-2 border border-black rounded-md'}),
+            'email': forms.TextInput(attrs={'class': 'w-full px-4 py-2 border border-black rounded-md'}),
         }
