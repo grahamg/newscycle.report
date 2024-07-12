78c6ee8cc7766804718b9278b82ab9a456859788
Graham Greenfield <grahamg@gmail.com>
2024-07-11 19:40:22 -0500

    Modify settings.py to get db settings from environmental variables.

--- bulletlist_report/bulletlist_report/settings.py
+++ bulletlist_report/bulletlist_report/settings.py
@@ -74,13 +74,24 @@ WSGI_APPLICATION = 'bulletlist_report.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+import os
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
