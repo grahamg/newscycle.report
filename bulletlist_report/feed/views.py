075f8698f9a3fbe61ae8e8af47e7dde74ab15389
Graham Greenfield <grahamg@gmail.com>
2024-07-18 03:36:44 -0500

    User feedback messages can be closed with an X icon. Add feedback message for successful update to subscriptions.

--- bulletlist_report/feed/views.py
+++ bulletlist_report/feed/views.py
@@ -3,6 +3,7 @@ import xml.etree.ElementTree as ET
 
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 from django.db.models import ObjectDoesNotExist
@@ -61,15 +62,13 @@ def subscriptions(request):
         if form.is_valid():
             selected_feeds = form.cleaned_data['feeds']
             user = request.user
-
             # Remove existing subscriptions
             UserSubscription.objects.filter(user=user).delete()
-
             # Create new subscriptions
             for feed in selected_feeds:
                 UserSubscription.objects.create(user=user, feed=feed)
-
-            return redirect('index')  # Redirect to a success page or wherever appropriate
+            messages.success(request, 'Your feed subscriptions have been updated.')
+            return redirect('index')
     else:
         # Pre-select feeds the user is already subscribed to
         user_feeds = UserSubscription.objects.filter(user=request.user).values_list('feed', flat=True)
