5642658ac987e58a50adba2d9006359bfda0fc02
Graham Greenfield <grahamg@gmail.com>
2024-08-09 09:12:30 -0500

    Update subscriptions checkbox list to have each feed under the heading category.

--- bulletlist_report/feed/forms.py
+++ bulletlist_report/feed/forms.py
@@ -1,8 +1,12 @@
 from django import forms
 from .models import RSSFeed, UserSubscription
+from collections import defaultdict
+from django.utils.safestring import mark_safe
+
 
 class OPMLUploadForm(forms.Form):
     opml_file = forms.FileField()
+
 
 class SubscriptionForm(forms.ModelForm):
     class Meta:
@@ -14,3 +18,21 @@ class SubscriptionForm(forms.ModelForm):
         widget=forms.CheckboxSelectMultiple,
         required=True
     )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Group feeds by category
+        grouped_feeds = defaultdict(list)
+        for feed in self.fields['feeds'].queryset:
+            category = feed.category.name if feed.category else 'Uncategorized'
+            grouped_feeds[category].append((feed.id, feed.title))
+        
+        # Generate choices with headings and padding
+        grouped_choices = []
+        for category, feeds in grouped_feeds.items():
+            category_heading = f'<strong style="padding-top: 10px; display: block;">{category}</strong>'
+            grouped_choices.append((mark_safe(category_heading), feeds))
+        
+        # Update the feeds field choices
+        self.fields['feeds'].choices = grouped_choices
+
